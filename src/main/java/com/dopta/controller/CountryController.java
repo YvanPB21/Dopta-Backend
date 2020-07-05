9f6c22e23ec428e073da2b70db444cb705eb7eef
YvanPB21 <yvanpb.2000.yp@gmail.com>
2020-07-05 01:51:57 -0500

    adsf

--- src/main/java/com/dopta/controller/CountryController.java
+++ src/main/java/com/dopta/controller/CountryController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin
 @RestController
 @RequestMapping("/api")
 public class CountryController {
